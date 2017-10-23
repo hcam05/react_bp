65a2592683ec65c4bf1140df1977deca523edb23
hcam05 <hcam05@gmail.com>
2017-10-23 07:26:24 -0700

    configuring eslint

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter from './components/Counter';
- 
+
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
     React.createElement(Counter),
